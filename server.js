48d53509c4c6e46c2318cf6cfaa3f45192664662
Aman <amansharma965@gmail.com>
2018-10-28 21:57:58 -0700

    final

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
+// ==============================================================================
+// DEPENDENCIES
+// ==============================================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-var methodOverride = require('method-override');
 
-// Import routes and give the server access to them.
-var burgersController = require("./controllers/burger_controller.js");
-// Import the model (burger.js) to use its database functions.
-var burger = require("./models/burger.js");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
-
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(methodOverride('X-HTTP-Method-Override'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
-// Handlebars
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", function(req, res) {
-	burger.selectAll(function(data) {
-		var hbsObject = {
-			burgers: data
-		};
-		console.log(hbsObject);
-		res.render("index", hbsObject);
-	});
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-app.use("/api/burgers", burgersController);
-
-app.listen(port);
